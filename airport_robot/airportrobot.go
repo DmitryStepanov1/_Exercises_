46c9776d9bab8a281b9861eb976d5a96989d4b09
DmitryStepanov1 <ds.dstepanov@gmail.com>
2023-07-12 18:56:11 +0300

    initial commit

---
+++ airport_robot/airportrobot.go
@@ -0,0 +1,31 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+type Italian struct{}
+type Portuguese struct{}
+
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s : %s", greeter.LanguageName(), greeter.Greet(name))
+}
+
+func (ital Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (ital Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+func (porto Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (porto Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Ola %s!", name)
+}
